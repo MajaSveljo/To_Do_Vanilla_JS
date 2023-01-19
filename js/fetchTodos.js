6f5694611b7717e986ac6755e736b1eedf9bb665
MajaSveljo <majasveljo@gmail.com>
2023-01-19 20:17:32 +0100

    added fetching of todos

--- js/fetchTodos.js
+++ js/fetchTodos.js
@@ -1,7 +1,13 @@
-const fetchTodos = (cb) => {
+const fetchTodos = (container) => {
   fetch("https://jsonplaceholder.typicode.com/todos")
     .then((response) => response.json())
-    .then((data) => cb(data))
+    .then((data) => {
+      for (let i = 0; i < 11; i++) {
+        let li = document.createElement("li");
+        li.innerHTML = `<label><input type='checkbox' id='${data[i].id}' name='${data[i].title}' value='${data[i].title}' /> ${data[i].title}</label>`;
+        container.append(li);
+      }
+    })
     .catch((error) => console.log(error));
 };
 
--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
 import { list } from "./renderTodosList.js";
 import { fetchTodos } from "./fetchTodos.js";
 
-const testFn = (data, container) => {
-  container = data;
-};
-
-let test;
-fetchTodos(testFn(test));
-console.log(test);
+fetchTodos(list);
