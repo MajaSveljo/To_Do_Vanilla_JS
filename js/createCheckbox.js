25d1b1968247c5e23eec99e8989c7575ffa2c208
MajaSveljo <majasveljo@gmail.com>
2023-01-31 10:03:19 +0100

    added some list styling

--- js/createCheckbox.js
+++ js/createCheckbox.js
@@ -1,6 +1,7 @@
 const createCheckbox = (data) => {
   let li = document.createElement("li");
-  li.innerHTML = `<label><input type='checkbox' id='${data.id}' name='${data.title}' value='${data.title}' /> ${data.title}</label>`;
+  li.className = "todo-item";
+  li.innerHTML = `<label class="todo-label"><input class="todo-checkbox" type='checkbox' id='${data.id}' name='${data.title}' value='${data.title}' /> ${data.title}</label><span class="delete-todo">X</span>`;
   return li;
 };
 
